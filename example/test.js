a57ce635877e76ad59b601a2e8d8e871328c5b1c
alex walters <tritium@sdamon.com>
2016-08-24 05:58:18 -0400

    dirname?

--- example/test.js
+++ example/test.js
@@ -3,7 +3,7 @@ const dcc = require('../lib/dcc');
 
 client = new irc.Client('irc.sorcery.net', 'Dueterium', {channels: ['#scram']})
 client.addListener('join#scram', () => {
-    dcc.sendFile(client, 'Tritium', 'example/data.txt', () => {
+    dcc.sendFile(client, 'Tritium', __dirname + '/data.txt', () => {
         client.disconnect('file sent');
         process.exit();
     });
