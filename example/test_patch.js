3ca5c74221d27efee34707e0ecaf7b92be9961a6
alex walters <tritium@sdamon.com>
2016-08-24 06:06:31 -0400

    Patching might be a bad idea...

---
+++ example/test_patch.js
@@ -0,0 +1,12 @@
+const irc = require('irc');
+const dcc = require('../lib/dcc');
+
+dcc.patch()
+
+client = new irc.Client('irc.sorcery.net', 'Dueterium', {channels: ['#scram']})
+client.addListener('join#scram', () => {
+    client.sendFile(client, 'Tritium', __dirname + '/data.txt', () => {
+        client.disconnect('file sent');
+        process.exit();
+    });
+});
