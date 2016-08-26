92853e764e0cb4434b61741427f881e1808bb5ed
alex walters <tritium@sdamon.com>
2016-08-26 00:46:54 -0400

    Accept chat?

--- example/test_chat.js
+++ example/test_chat.js
@@ -13,9 +13,26 @@ client.addListener('ctcp-privmsg', (from, to, text, message) => {
                     if (line.startsWith('exit')) {
                         chat.say("Bye!!")
                         chat.disconnect();
+                    } else {
+                        chat.say("You said: " + line)
                     }
                 });
             });
+            break;
+        case "dcc":
+            var args = dcc.parseDCC(text);
+            if (args.type == 'chat') {
+                dcc.acceptChat(client, args.addr, args.port, (chat) => {
+                    chat.on('line', (line) => {
+                        if (line.startsWith('exit')) {
+                            chat.say("Bye!!")
+                            chat.disconnect();
+                        } else {
+                            chat.say("You said: " + line)
+                        }
+                    });
+                });
+            }
             break;
         case "exit":
             client.disconnect("goodbye!");
