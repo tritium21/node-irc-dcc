8ce573be816b5e103e5a1c2cc016cb5e162af33a
alex walters <tritium@sdamon.com>
2016-08-28 04:07:42 -0400

    acceptSend refactored.

--- example/test.js
+++ example/test.js
@@ -9,11 +9,26 @@ client.addListener('ctcp-privmsg', (from, to, text, message) => {
     var cmd = text.split(" ")[0].toLowerCase();
     switch (cmd) {
         case "send":
-            dcc.sendFile(from, __dirname + '/data.txt', (err) => { if (err) { console.log(err); } });
+            dcc.sendFile(from, __dirname + '/data.txt', (err) => {
+                if (err) {
+                    client.notice(from, err);
+                }
+            });
             break;
         case "exit":
             client.disconnect("goodbye!");
             process.exit();
     }
 
+});
+client.on('dcc-send', (from, args, message) => {
+    var ws = fs.createWriteStream(__dirname + "/" + args.filename)
+    console.log(args);
+    dcc.acceptSend(from, args.host, args.port, args.filename, args.length, (err, filename, con) => {
+        if (err) {
+            client.notice(from, err);
+            return;
+        }
+        con.pipe(ws);
+    });
 });
