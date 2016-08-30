b43f2e02825c359c00f0cba2012103f000ebdf40
alex walters <tritium@sdamon.com>
2016-08-30 07:12:33 -0400

    Try coveralls

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/ip/index.d.ts" />
 /// <reference path="globals/irc/index.d.ts" />
+/// <reference path="globals/istanbul/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/pegjs/index.d.ts" />
