64b4bed66bfdc5265585fafc1d2cc471a43e85dc
alex walters <tritium@sdamon.com>
2016-08-30 04:26:56 -0400

    Byte order mark, part 1

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="globals/ip/index.d.ts" />
 /// <reference path="globals/irc/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/mock-require/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/pegjs/index.d.ts" />
 /// <reference path="globals/simple-mock/index.d.ts" />
