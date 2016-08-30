fcfade2327cc49782220388f32414800ade9209c
alex walters <tritium@sdamon.com>
2016-08-30 03:47:18 -0400

    Test parseDCC

--- test/chat.test.js
+++ test/chat.test.js
@@ -11,38 +11,31 @@ describe('Chat', () => {
     };
 
     afterEach(() => {
+        simple.restore()
         stub_con.addListener.reset();
         stub_con.write.reset();
         stub_con.end.reset();
     });
-    describe('listen', () => {
-        it('should listen for data events', () => {
-            var chat = new Chat(stub_con);
-            assert.equal(stub_con.addListener.lastCall.args[0], "data");
-        });
+    it('should listen for data events', () => {
+        var chat = new Chat(stub_con);
+        assert.equal(stub_con.addListener.lastCall.args[0], "data");
     });
-    describe('emit', () => {
-        it('should emit lines', () => {
-            var chat = new Chat(stub_con);
-            simple.mock(chat, "emit");
-            var hnd = stub_con.addListener.lastCall.args[1];
-            hnd("this is a line\r\n");
-            assert.equal(chat.emit.lastCall.args[0], "line");
-            assert.equal(chat.emit.lastCall.args[1], "this is a line");
-        });
+    it('should emit lines', () => {
+        var chat = new Chat(stub_con);
+        simple.mock(chat, "emit");
+        var hnd = stub_con.addListener.lastCall.args[1];
+        hnd("this is a");
+        hnd(" line\r\n");
+        assert.deepEqual(chat.emit.lastCall.args, ["line", "this is a line"]);
     });
-    describe('#say', () => {
-        it('should write without error', () => {
-            var chat = new Chat(stub_con);
-            chat.say("this is a test")
-            assert.equal(stub_con.write.lastCall.args, "this is a test\r\n");
-        });
+    it('should write without error', () => {
+        var chat = new Chat(stub_con);
+        chat.say("this is a test")
+        assert.equal(stub_con.write.lastCall.args, "this is a test\r\n");
     });
-    describe('#disconnect', () => {
-        it('should disconnect with #end', () => {
-            var chat = new Chat(stub_con);
-            chat.disconnect();
-            assert.equal(stub_con.end.callCount, 1);
-        });
-    })
+    it('should disconnect with #end', () => {
+        var chat = new Chat(stub_con);
+        chat.disconnect();
+        assert.equal(stub_con.end.callCount, 1);
+    });
 });
---
+++ test/dcc.test.js
@@ -0,0 +1,52 @@
+﻿var assert = require('assert');
+var simple = require('simple-mock');
+var DCC = require('../lib/dcc');
+
+describe('parseDCC', function() {
+    it('should return null', function () {
+        var args = DCC.parseDCC('This is not a dcc message')
+        assert.equal(args, null);
+    })
+    it('should return a send', function () {
+        var args = DCC.parseDCC('DCC SEND foo_bar.pdf 3232235876 6500 95871')
+        var expected = {
+            type: 'send',
+            long: 3232235876,
+            host: '192.168.1.100',
+            port: 6500,
+            filename: 'foo_bar.pdf',
+            length: 95871
+        }
+        assert.deepEqual(args, expected);
+    })
+    it('should return a chat', function () {
+        var args = DCC.parseDCC('DCC CHAT chat 3232235876 6500')
+        var expected = {
+            type: 'chat',
+            long: 3232235876,
+            host: '192.168.1.100',
+            port: 6500,
+        }
+        assert.deepEqual(args, expected);
+    })
+    it('should return an accept', function () {
+        var args = DCC.parseDCC('DCC ACCEPT foo_bar.pdf 6500 31565')
+        var expected = {
+            type: 'accept',
+            port: 6500,
+            filename: 'foo_bar.pdf',
+            position: 31565
+        }
+        assert.deepEqual(args, expected);
+    })
+    it('should return a resume', function () {
+        var args = DCC.parseDCC('DCC RESUME foo_bar.pdf 6500 31565')
+        var expected = {
+            type: 'resume',
+            port: 6500,
+            filename: 'foo_bar.pdf',
+            position: 31565
+        }
+        assert.deepEqual(args, expected);
+    })
+})
